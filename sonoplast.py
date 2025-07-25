56cc3f269ffc7922f549cebea6d2973bf0013f14
Felipe Leal <felipesleal@gmail.com>
2025-07-25 18:03:22 +0100

    play button

--- sonoplast.py
+++ sonoplast.py
@@ -1,17 +1,31 @@
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
 
 Window.size = (1280, 720)
 Window.title = "SonoBlast"
 Window.fullscreen = True
 
-class SonoplastUI(FloatLayout):
-    def play(self):
-        print("<INFO> Play pressed")
 
-    def stop(self):
-        print("<WARN> Emergency Stop!")
+class PlayButton(ButtonBehavior, Image):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.source = "images/play.png"
+        self.state_on = False
+    def on_press(self):
+        
+        self.state_on = not self.state_on
+        if self.state_on:
+            self.source = "images/play.png"
+            print("<INFO> Play enabled")
+        else:
+            self.source = "images/pause.png"
+            print("<INFO> Pause enabled")
+
+class SonoplastUI(FloatLayout):
+    pass
 
 class SonoplastApp(App):
     def build(self):
