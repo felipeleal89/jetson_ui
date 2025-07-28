3173bd2723061339e1e42a3d4001204f5ddfbf09
Felipe Leal <felipesleal@gmail.com>
2025-07-28 10:55:17 +0100

    for and rev buttons

--- sonoplast.py
+++ sonoplast.py
@@ -1,28 +1,53 @@
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
+from kivy.config import Config
 from kivy.uix.image import Image
-from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.button import ButtonBehavior
 
 Window.size = (1280, 720)
 Window.title = "SonoBlast"
-Window.fullscreen = True
 
 
 class PlayButton(ButtonBehavior, Image):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.source = "images/play.png"
         self.state_on = False
     def on_press(self):
-        
         self.state_on = not self.state_on
-        if self.state_on:
-            self.source = "images/play.png"
-            print("<INFO> Play enabled")
-        else:
-            self.source = "images/pause.png"
-            print("<INFO> Pause enabled")
+        self.source = (
+            "images/pause.png" if self.state_on else "images/play.png"
+        )
+
+class ReverseButton(ButtonBehavior, Image):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.state_on = False
+    def on_press(self):
+        self.state_on = not self.state_on
+        self.source = (
+            "images/rev_en.png" if self.state_on else "images/rev_dis.png"
+        )
+    def on_release(self):
+        self.state_on = not self.state_on
+        self.source = (
+            "images/rev_en.png" if self.state_on else "images/rev_dis.png"
+        )
+
+class ForwardButton(ButtonBehavior, Image):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.state_on = False
+    def on_press(self):
+        self.state_on = not self.state_on
+        self.source = (
+            "images/forw_en.png" if self.state_on else "images/forw_dis.png"
+        )
+    def on_release(self):
+        self.state_on = not self.state_on
+        self.source = (
+            "images/forw_en.png" if self.state_on else "images/forw_dis.png"
+        )
 
 class SonoplastUI(FloatLayout):
     pass
